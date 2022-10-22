3f01688d642459145d5bc1246a5a863a8f81051c
JESIKA PUTRI <97590486+JESIKAPUTR123@users.noreply.github.com>
2022-10-22 13:27:45 +0700

    Add files via upload

---
+++ style/StyleCheckOut.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    row:{
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        backgroundColor: 'powderblue',
+        
+    },
+    upper:{
+        alignItems:'center',
+        //justifyContent:'center',
+        backgroundColor: 'powderblue',
+        height: 150,
+    },
+    textSubtittle:{
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    textTitle:{
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+    marginMain:{
+        marginVertical: 20,
+        marginHorizontal: 10,
+    },
+    viewCard:{
+        flexDirection: "row",
+        padding: 10,
+    },
+    viewImage:{
+        flex: 1,
+    },
+});
+export default styles;
